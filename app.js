954c7d55573458172c03900e0ea745d270cdef02
Brian Huston <hbriandrums@gmail.com>
2018-09-12 13:05:19 -0700

    fixed displayCategory function.

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 $(document).ready(function() {
 
+  displayCategoryButtons();
+
+  function displayCategoryButtons() {
+    let buttonsArray = [];
+
+    for (var i = 0; i < localStorage.length; i++) {
+      buttonsArray.push(localStorage.key(i));
+    }
+
+    return buttonsArray.map(el => {
+
+      storedButton = $('<button class="category" data-name="' + el + '">' + el + '</button>');
+      $(".button-drop").append(storedButton);
+      
+      $(storedButton).on("click", function() {
+        let itemHtml = '<div class="display-item" data-storage-key="'+el+'"> ' + 
+        localStorage.getItem(el) + '</div>';
+
+        $(".display").html(itemHtml);
+      });
+    })
+
+    
+    // $(storedButton).on("click", function() {
+    //   let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + 
+    //   inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
+
+    //   $(".display").html(itemHtml);
+    // });
+
+  }
+
+  // $(".category").on("click", function() {
+
+  // }) 
+
 
   $(".add-text-btn").on("click", function(){
-    console.log('hi');
+    // e.preventDefault();
     // store values
     let inputKey = $(".user-input-title").val();
     let inputValue = $(".user-input-body").val();
@@ -11,17 +47,31 @@ $(document).ready(function() {
     $(".user-input-title").val("");
     $(".user-input-body").val("");
 
-    console.log(inputKey, inputValue);
-
+    // set key and value in local storage
     localStorage.setItem(inputKey, inputValue);
+
+    // let listItems = localStorage.setItem('items') ? 
+    // JSON.parse(localStorage.setItem('items')) : [];
+
+    // console.log(listItems)
     
     //create a button for each category
-    let categoryButton = $('<button class="category" data-name="' + inputKey + '">' + inputKey + '</button>');
+    let categoryButton = $('<button class="category" data-name="' + 
+      inputKey + '">' + inputKey + '</button>');
+
+    // drop button below title
     $(".button-drop").append(categoryButton)
+
+    $(categoryButton).on("click", function() {
+      let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + 
+      localStorage.getItem(inputKey) + '</div>';
+
+      $(".display").html(itemHtml);
+    });
 
     // let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
     // $(".display").html(itemHtml);
-    console.log(localStorage);
+    // console.log(localStorage);
     // how can we delegate this event to the outer html node?
     // https://learn.jquery.com/events/event-delegation/
 
@@ -38,8 +88,6 @@ $(document).ready(function() {
     });
 
   });
-
-
 
    // TODO add back in later
    // $(".user-input").on("keyup", function(){
