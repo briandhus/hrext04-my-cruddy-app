c8640077d07b19b1c6d5ccc5e762681d11d36848
Brian Huston <hbriandrums@gmail.com>
2018-09-13 11:08:41 -0700

    changed html layout for lists. Added onkeyup for list items

--- app.js
+++ app.js
@@ -3,42 +3,47 @@ $(document).ready(function() {
   displayCategoryButtons();
 
   function displayCategoryButtons() {
+
     let buttonsArray = [];
 
     for (var i = 0; i < localStorage.length; i++) {
-      buttonsArray.push(localStorage.key(i));
+
+      buttonsArray.push([localStorage.key(i), localStorage.getItem(localStorage.key(i))]);
+
     }
 
     return buttonsArray.map(el => {
 
-      storedButton = $('<button class="category" data-name="' + el + '">' + el + '</button>');
-      $(".button-drop").append(storedButton);
-      
-      $(storedButton).on("click", function() {
-        let itemHtml = '<div class="display-item" data-storage-key="'+el+'"> ' + 
-        localStorage.getItem(el) + '</div>';
+      let obj = {};
 
-        $(".display").html(itemHtml);
+      el.forEach(item => {
+        obj[el[0]] = item;
       });
-    })
 
-    
-    // $(storedButton).on("click", function() {
-    //   let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + 
-    //   inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
+      for (let key in obj) {
 
-    //   $(".display").html(itemHtml);
-    // });
+        let storedButton = $('<button class="category" data-name="' + key + '">' + 
+          key + '</button>');
 
+        $(".button-drop").append(storedButton);
+
+        $(storedButton).on("click", function() {
+
+          let listHtml = '<div class="display-category" data-storage-key="'+key+'"> ' + 
+          key + ': ' + '</div>';
+
+          let itemHtml = '<div class="display-item" data-storage-key="'+key+'"> ' + 
+          localStorage.getItem(key) + '</div>';
+
+          $(".display").html(listHtml + itemHtml);
+
+        });
+      }
+    });
   }
 
-  // $(".category").on("click", function() {
-
-  // }) 
-
-
   $(".add-text-btn").on("click", function(){
-    // e.preventDefault();
+
     // store values
     let inputKey = $(".user-input-title").val();
     let inputValue = $(".user-input-body").val();
@@ -52,8 +57,6 @@ $(document).ready(function() {
 
     // let listItems = localStorage.setItem('items') ? 
     // JSON.parse(localStorage.setItem('items')) : [];
-
-    // console.log(listItems)
     
     //create a button for each category
     let categoryButton = $('<button class="category" data-name="' + 
@@ -63,15 +66,14 @@ $(document).ready(function() {
     $(".button-drop").append(categoryButton)
 
     $(categoryButton).on("click", function() {
+      let listHtml = '<div class="display-category" data-storage-key="'+inputKey+'"> ' + 
+      inputKey + ': ' + '</div><br>';
       let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + 
       localStorage.getItem(inputKey) + '</div>';
 
-      $(".display").html(itemHtml);
+      $(".display").html(listHtml + itemHtml);
     });
 
-    // let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
-    // $(".display").html(itemHtml);
-    // console.log(localStorage);
     // how can we delegate this event to the outer html node?
     // https://learn.jquery.com/events/event-delegation/
 
@@ -90,21 +92,22 @@ $(document).ready(function() {
   });
 
    // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
+  $(".user-input-body").on("keyup", function(){
+    let inputKey = $(".user-input-title").val();
+    let inputValue = $(".user-input-body").val();
+    localStorage.setItem(inputKey, inputValue);
+    $(".display").text(localStorage.getItem(inputKey));
+  });
 
-   $(".del-text-btn").on("click", function() {
-     window.confirm('Delete item?'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
-     $(".user-input-title").val("");
-     $(".user-input-body").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
-   });
+  $(".del-text-btn").on("click", function() {
+   window.confirm('Delete item?'); // maybe change to a window.confirm
+   localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
+   $(".user-input-title").val("");
+   $(".user-input-body").val("");
+   // clearing display? what if I have multiple items?
+   // after item is removed from local storage, redisplay items from local storage
+   // refresh from storage?
+  });
 
 
    // iterative approach to adding items
